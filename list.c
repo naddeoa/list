22a65e5179248b3729004b423f15d296d51b3cab
Anthony Naddeo <anthony.naddeo@gmail.com>
2012-04-02 00:55:41 -0400

    initial commit

---
+++ list.c
@@ -0,0 +1,365 @@
+#include "list.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* Private function that swaps two adjacent nodes.
+* The nodes should be in order, with n1 before n2 in
+* the list.
+* @param lst The list that these nodes belong to. This is
+* needed to update the head and tail pointers if they are
+* effected.
+* @param n1 The node that occurs earlier in the list.
+* @param n2 The node that occurs after n1.
+*/
+void _swap(list* lst, node* n1, node* n2){
+	node* n1Prev = n1->prev;
+	node* n1Next = n1->next;
+	node* n2Prev = n2->prev;
+	node* n2Next = n2->next;
+
+	int adjacent = n1->next == n2 ? 1 : 0;
+
+	if(lst->head == n1)
+		lst->head = n2;
+
+	if(lst->tail == n2)
+		lst->tail = n1;
+
+	n1->prev = adjacent ? n2 : n2Prev;
+	n1->next = n2Next;
+
+	n2->next = adjacent ? n1 : n1Next;
+	n2->prev = n1Prev;
+
+	if(n1Prev)
+		n1Prev->next = n2;
+	if(n1Next && !adjacent)
+		n1Next->prev = n2;
+	if(n2Next)
+		n2Next->prev = n1;
+	if(n2Prev && !adjacent)
+		n2Prev->next = n1;
+}
+
+/**
+* Private function that gets a node by index. This 
+* function returns the node, whereas teh get() function
+* returns the data in the node.
+* @param lst A pointer to the list to get from.
+* @param index The index.
+* @return Pointer to the node at index index
+*/
+node* _get(list* lst, int index){
+	
+	if(index >= lst->length || index < 0){
+		printf("index %i out of bounds\n", index);
+		exit(1);
+	}
+	
+	node* n = lst->head;
+	int i;
+	for(i=0; i<index; i++){
+		n = n->next;
+	}
+
+	return n;
+}
+
+/**
+* Pop the first item off the list and return it.
+* This is a private version of pop() that returns
+* a node instead of a void* to data. Used to make
+* cycling more efficient.
+* @param lst A pointer to the list to pop from.
+* @return A node* pointing to the popped node.
+*/
+node* _pop_node(list* lst) {
+
+	if(lst->length == 0){
+		printf("called 'pop' on an empty list.\n");
+		exit(1);
+	}
+
+	node* popped = lst->head;
+	lst->head = popped->next; 
+	lst->head->prev = 0; //head node has no previous node
+	lst->length--;
+	return popped;
+}
+
+/**
+* Private version of the append() function. This
+* function deals with nodes instead of data for
+* more efficient cycles.
+* @param lst A pointer to the list to append to.
+* @param n A pointer to the node to append.
+*/
+void _append_node(list* lst, node* n) {
+
+	if (lst->length == 0) {
+		lst->head = n;
+		lst->tail = n;
+	} else {
+		node* oldTail = lst->tail;
+
+		oldTail->next = n;
+		n->next = 0;
+		n->prev = oldTail;
+
+		lst->tail = n;
+	}
+
+	lst->length++;
+}
+
+
+void* pop(list* lst) {
+
+	if(lst->length == 0){
+		printf("called 'pop' on list an empty list.\n");
+		exit(1);
+	}
+
+	node* popped = lst->head;
+	lst->head = popped->next;
+	lst->head->prev = 0;
+	lst->length--;
+	return popped->data;
+}
+
+void* get(list* lst, int index){
+	
+	if(index >= lst->length || index < 0){
+		printf("index %i out of bounds\n", index);
+		exit(1);
+	}
+	
+	node* n = lst->head;
+	int i;
+	for(i=0; i<index; i++){
+		n = n->next;
+	}
+
+	return n->data;
+}
+
+void* rm(list* lst, int index){
+
+	if(lst->length <= index){
+		printf("list does not have that many elements, cannot remove");
+		exit(1);
+	}
+
+	if(index == 0)
+		return pop(lst);
+	else{
+		int i;
+		node* n = lst->head;
+		for(i = 0; i < index-1; i++){
+			n = n->next;
+		}
+
+		
+		node* rm = n->next; // node to be removed
+		n->next = rm->next;
+		rm->next->prev = n;
+
+		if(index == lst->length-1)
+			lst->tail = n;
+
+		lst->length--;
+		return rm->data;
+	}
+
+}
+
+void cycle(list* lst){
+	if(lst->length > 1){
+		_append_node(lst, _pop_node(lst));
+	}
+}
+
+
+void append(list* lst, void* data) {
+
+	node* newNode = (node*) malloc(sizeof(node));
+	newNode->data = data;
+
+	if(lst->length == 0) {
+		lst->head = newNode;
+		lst->tail = newNode;
+	}else{
+		node* oldTail = lst->tail;
+
+		oldTail->next = newNode;
+		newNode->next = 0;
+		newNode->prev = oldTail;
+
+		lst->tail = newNode;
+	}
+
+	lst->length++;
+}
+
+
+void printList(list* lst){
+	node* n = lst->head;
+	pair* p;
+	printf("[ ");
+	int i;
+	for(i=0; i<lst->length; i++){
+		p = (pair*)n->data;
+		printf("%s:%s, ", p->key, p->val);
+		n = n->next;
+	}
+	printf("]\n\n");
+}
+
+void swap(list* lst, int index1, int index2){
+	if(index1 < 0 || index1 >= lst->length ||
+		 index2 < 0 || index2 >= lst->length){
+		printf("index out of bounds\n");
+		exit(1);
+	}
+	
+	node* n1 = _get(lst, index1);
+	node* n2 = _get(lst, index2);
+	if(index1 < index2)
+		_swap(lst,n1,n2);
+	else
+		_swap(lst,n2,n1);
+}
+
+
+/////////////// START LIST ITERATOR FUNCTIONS /////////////////
+
+
+list_iterator* get_iterator(list* lst){
+	list_iterator* iter = (list_iterator*)malloc(sizeof(list_iterator));
+	iter->current = lst->head;
+	iter->lst = lst;
+	iter->index = 0;
+	return iter;
+}
+
+int next(list_iterator* iter){
+	if(!iter->current->next)
+		return iter->index;
+	
+	iter->current = iter->current->next;
+	iter->index++;
+	return iter->index;
+}
+
+
+int prev(list_iterator* iter){
+	if(!iter->current->prev)
+		return iter->index;
+	
+	iter->current = iter->current->prev;
+	iter->index--;
+	return iter->index;
+}
+
+
+void* data(list_iterator* iter){
+	return iter->current->data;
+}
+
+void* find(list_iterator* iter, int index){
+	//ensure the index is in bounds
+	if(index >= iter->lst->length || index < 0){
+		printf("index out of bounds\n");
+		exit(1);
+	}
+
+	//determine if the iterator should move forwards or backwards
+	int forward = index > iter->index ? 1 : 0;
+	int diff = forward ? index - iter->index : iter->index - index;
+	
+	//small optimization
+	if(!forward && index < diff){
+		//it is faster to reset iter to the head
+		//and move forward, instead of backwards
+		iter->current = iter->lst->head;
+		iter->index = 0;
+		forward = !forward;
+		diff = index;
+	}
+
+	//small optimization
+	if(forward && (iter->lst->length - index - 1) < diff){
+		//it is faster to reset iter to the tail
+		//and move backwards, instead of forwards
+		iter->current = iter->lst->tail;
+		iter->index = iter->lst->length - 1;
+		forward = !forward;
+		diff = (iter->lst->length - index - 1);
+	}
+
+	//create a pointer to either the next() or prev() functions.
+	//Which one depends on the index being before or after our
+	//iterators current position. 
+	int (*action)(list_iterator*) = forward ? next : prev;
+
+	//find the element
+	int i;
+	for(i=0; i < diff; i++){
+		action(iter);
+	}
+	
+	return data(iter);
+}
+
+int str_order(void* vstr1, void* vstr2){
+	//cast the void* to char*. This function only
+	//compares strings
+	char* str1 = (char*)vstr1;
+	char* str2 = (char*)vstr2;
+	int i;
+	//compare the strings char by char
+	for(i = 0; str1[i] != 0 && str2[i] != 0; i++){
+		if(str1[i] < str2[i])
+			return 0;
+		else if(str1[i] > str2[i])
+			return 1;
+	}
+
+	//if it got this far, then the shorter of the
+	//two strings must be the smaller one alphabetically
+	if(strlen(str1) > strlen(str2))
+		return 1;
+	else if(strlen(str2) > strlen(str1))
+		return 0;
+	else //they are equal
+		return 2;
+}
+
+
+void* str_order_data(void* data){
+	pair* p = (pair*)data;
+	return p->key;
+}
+
+void bubble_sort(list_iterator* iter, int(*cmp_func)(void*,void*),
+                void*(*data_func)(void*)){
+	int i,j;
+	void* p1;
+	void* p2;
+	for(i = 0; i < iter->lst->length-1; i++){
+		for(j = 0; j < iter->lst->length-1; j++){
+			find(iter, j);	
+			p1 = str_order_data(iter->current->data);
+			p2 = str_order_data(iter->current->next->data);
+			if( cmp_func( p1, p2)){
+				_swap(iter->lst, iter->current, iter->current->next);
+				iter->index++;
+			}
+		}
+	}
+
+}
+
+
+
---
+++ list.h
@@ -0,0 +1,209 @@
+/** @file list.h
+* @brief Declarations for the C list. The list is a struct
+* that represents a linked list.
+*/
+
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+
+/**
+* The node struct represents an element of
+* a list.
+*/
+typedef struct node_t{
+	struct node_t* prev;
+	struct node_t* next;
+	void* data;
+}node;
+
+/**
+* A list struct that contains a length, 
+* head node, and tail node. Methods in this
+* header operate mostly on this struct.
+*/
+typedef struct list_t{
+	int length;
+	node* head;
+	node* tail;
+}list;
+
+/**
+* An iterator for the linked list.
+*/
+typedef struct iter_t{
+	int index;
+	list* lst;
+	node* current;
+}list_iterator;
+
+/**
+* The pair type is a simple struct containing
+* a key and val char*. This is included as a 
+* possible option for the void* with each node.
+*/
+typedef struct pair_t{
+	char* key;
+	char* val;
+}pair;
+
+/**
+* Pop the first item off the list and return it.
+* @param lst A pointer to the list to pop from.
+* @return A node* pointing to the popped node.
+*/
+void* pop(list* lst);
+
+/**
+* Index into a list and return the data at that
+* node.
+* @param lst The list to index into.
+* @param index The index.
+* @return void* to the data in the element at index.
+*/
+void* get(list* lst, int index);
+
+/**
+* Remove an item from the list. This method is similar
+* to pop() but you may specify the node index.
+* @param lst A pointer to the list to remove from.
+* @param index The index of the item to remove.
+* @return A node* pointing to the removed node.
+*/
+void* rm(list* lst, int index);
+
+/**
+* Cycle the list, moving the head to the tail and
+* making the item after the original head the new head.
+* @param lst A pointer to the list to cycle.
+*/
+void cycle(list* lst);
+ 
+/**
+* Append data to a list.
+* @param lst A pointer to the list to append to.
+* @param data A pointer to the data to append.
+*/
+void append(list* lst, void* data);
+
+
+void printList(list* lst);
+
+/**
+* Swap two elements in the list. The order of the
+* parameters does not matter. That is, index1 does
+* not have to be less than index2.
+* @param lst The list to swap elements of.
+* @param index1 The index of the first element to swap.
+* @param index2 The index of the second element to swap.
+*/
+void swap(list* lst, int index1, int index2);
+
+
+
+/////////////// START LIST ITERATOR FUNCTIONS /////////////////
+
+
+/**
+* Create a new iterator for a linked list. The iterator
+* will be pointing to the head of the list.
+* @param lst A list to generate an iterator for
+* @return A pointer to a new list_iterator struct. 
+*/
+list_iterator* get_iterator(list* lst);
+
+/**
+* Iteratore to the next element in a list.
+* @param iter A list_iterator* to the list_iterator
+* to iterate forward.
+* @return The current index of the iterator. If there were
+* no more elements in the list then this will not change
+* between calls.
+*/
+int next(list_iterator* iter);
+
+/**
+* Iteratore to the previous element in a list.
+* @param iter A list_iterator* to the list_iterator
+* to iterate backwards.
+* @return The current index of the iterator. If there were
+* no more elements in the list then this will not change
+* between calls.
+*/
+int prev(list_iterator* iter);
+
+/**
+* Get the data of the current element in the list.
+* @param iter A list_iterator.
+* @return A void* to the data that is inside of the current
+* elemenet.
+*/
+void* data(list_iterator* iter);
+
+/**
+* This finds and returns the data inside of the element
+* at the specified index. This function is similar to the
+* general purpose get() function that operates on a list,
+* but it uses an iterator to search more efficiently (as
+* opposed to iterating through the entire list one by one 
+* each time an element is desired). This function includes
+* small optimizations that set the iterator to the head/tail
+* if it is faster to start from there, so you will not end
+* up iterating backwards through the entire list to get to index
+* 0 from the tail. This means that you will not have to use
+* get() at all if you are using an iterator.
+* @param iter The list_iterator struct that should be used.
+* @param index The index to find.
+* @return A void* to the data inside of the element at index.
+*/
+void* find(list_iterator* iter, int index);
+
+/**
+* Used for finding alphabetical order. The parameters
+* are void* to allow this to be used as one of the comparator
+* functions in the bubble_sort. 
+* @param str1 The first string to compare.
+* @param str2 The second string to compare.
+* @return 0 if str1 comes before str2 in alphabetical order.
+* 1 if str2 comes before str3 in alphabetical order.
+* 2 if they are equal.
+*/
+int str_order(void* str1, void* str2);
+
+/**
+* Generic function that extracts data from the void* in
+* nodes. This is used to enable generic sorts that work
+* on any data type, provided a comparator function and
+* data extracting function are passed into the sort.
+* @param data The void* in a node. This function is tailored
+* to the pair* type, extracting the char* key value from the 
+* data and returning it as a void* to be passsed into the
+* comparator function.
+* @return void* Some data inside the void* data within list
+* nodes. Currently, the char* key of the pair struct is returned
+* as I designed this to sort by key values in pairs.
+*/
+void* str_order_data(void* data);
+
+/**
+* Sort a list using one of its iterators.
+* @param iter An iterator over the target list
+* @param cmp_func A function pointer to a function that compares
+* to types. This function should return 0 if the first parameter
+* is less than the second parameter, and 1 if the second 
+* parameter is less than the first parameter. 2 is returned if they
+* are equal. This allows bubble sort in a generic way. Look at 
+* the str_order function for a model of how to make your own.
+* @param data_func A function that can be used to extract data
+* from the void* in each list node. The contents of the void* 
+* are unknown so a function must be provided to extract this
+* data as another void*, ready to be passed into the cmp_func.
+* This function should return the data that you wish to sort by
+* in the cmp_func.
+*/
+void bubble_sort(list_iterator* iter, int(*cmp_func)(void*, void*),
+                 void*(*data_func)(void*));
+
+
+#endif
